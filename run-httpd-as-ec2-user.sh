3d88cfc2d13743e819052d55296122d545d7dfcb
miya0001 <miya@wpist.me>
2015-07-04 17:47:52 +0900

    hhvm support

--- run-httpd-as-ec2-user.sh
+++ run-httpd-as-ec2-user.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-service monit stop
+hhvm --version
 
+if [ $? == 0 ]; then
+echo '{
+"nginx" : { "config" : { "user" : "ec2-user" } },
+"php" : { "config" : { "user" : "ec2-user" } },
+"hhvm" : { "enabled" : true, "config" : { "user" : "ec2-user" } },
+"run_list" : [ "recipe[amimoto]" ]
+}' > /opt/local/amimoto.json
+else
 echo '{
 "nginx" : { "config" : { "user" : "ec2-user" } },
 "php" : { "config" : { "user" : "ec2-user" } },
 "run_list" : [ "recipe[amimoto]" ]
 }' > /opt/local/amimoto.json
+fi
 
 /opt/local/provision
 chown -R ec2-user:nginx /var/www/vhosts

2c31b7641b2fd7c3deca814318a3269e31006066
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2015-07-04 17:34:47 +0900

    Update run-httpd-as-ec2-user.sh

--- run-httpd-as-ec2-user.sh
+++ run-httpd-as-ec2-user.sh
@@ -4,11 +4,11 @@ set -ex
 
 service monit stop
 
-echo "{
+echo '{
 "nginx" : { "config" : { "user" : "ec2-user" } },
 "php" : { "config" : { "user" : "ec2-user" } },
 "run_list" : [ "recipe[amimoto]" ]
-}" > /opt/local/amimoto.json
+}' > /opt/local/amimoto.json
 
 /opt/local/provision
 chown -R ec2-user:nginx /var/www/vhosts

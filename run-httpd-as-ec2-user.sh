6ed4441d1784efd6bbf22695edd8a277f3cc8113
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2015-07-04 17:34:10 +0900

    Update run-httpd-as-ec2-user.sh

--- run-httpd-as-ec2-user.sh
+++ run-httpd-as-ec2-user.sh
@@ -4,18 +4,11 @@ set -ex
 
 service monit stop
 
-sed -i -e "s/^user .*;$/user ec2-user ec2-user;/" /etc/nginx/nginx.conf
-sed -i -e "s/^listen.owner .*$/listen.owner = ec2-user/" /etc/php-fpm.d/www.conf
-sed -i -e "s/^listen.group .*$/listen.group = ec2-user/" /etc/php-fpm.d/www.conf
-sed -i -e "s/^user .*$/user = ec2-user/" /etc/php-fpm.d/www.conf
-sed -i -e "s/^group .*$/group = ec2-user/" /etc/php-fpm.d/www.conf
-sed -i -e "s/nginx:nginx/ec2-user:ec2-user/g" /etc/monit.d/php-fpm
-sed -i -e "s/nginx:nginx/ec2-user:ec2-user/g" /etc/monit.d/nginx
+echo "{
+"nginx" : { "config" : { "user" : "ec2-user" } },
+"php" : { "config" : { "user" : "ec2-user" } },
+"run_list" : [ "recipe[amimoto]" ]
+}" > /opt/local/amimoto.json
 
-chown -R ec2-user:ec2-user /var/www/vhosts
-chown -R ec2-user:ec2-user /var/tmp/php/session
-chown -R ec2-user:ec2-user /var/lib/nginx
-
-service php-fpm restart
-service nginx restart
-service monit start
+/opt/local/provision
+chown -R ec2-user:nginx /var/www/vhosts
